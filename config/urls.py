99da6e0b1ad2081cedc76fb877a781c7c09aa76a
Evgeniy-Dubinetc <edubinetc2@mail.ru>
2022-10-24 20:21:41 +0300

    lesson_2 run

--- config/urls.py
+++ config/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include # new
 from django.contrib import admin
 from django.urls import path
 
-from users.views import UsersAPIVieW
+from users.views import UsersAPIVieW # new
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/v1/userslist/', UsersAPIVieW.as_view()),
     
 ]
