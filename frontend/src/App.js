27187993c397ede73a18b2d78ca4a25c64cd8888
Evgeniy-Dubinetc <edubinetc2@mail.ru>
2022-10-24 22:06:19 +0300

    lesson_2 react run

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UsersList from './components/Users.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      'users': []
+    }
+  }
+  
+  componentDidMount() {
+    const users = [
+      {
+        'user_name': 'Стрелок',
+        'first_name': 'Игорь',
+        'last_name': 'Попов',
+        'email': 'efg777@mail.ru'
+      },
+      {
+        'user_name': 'Автор',
+        'first_name': 'Степан',
+        'last_name': 'Иванов',
+        'email': 'rrf111@mail.ru'
+      },
+    ]
+    this.setState(
+      {
+        'users': users
+      }
+    )
+  }
+  
+  render () {
+    return (
+      <div>
+        <UsersList users={this.state.users} />
+      </div>
+    )
+  }
 }
-
+  
 export default App;
--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,10 @@
-from rest_framework import serializers
+# from rest_framework import serializers
+from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import Users
 
 
-class UsersSerializer(serializers.ModelSerializer):
-
-
+# class UsersSerializer(serializers.ModelSerializer):
+class UsersModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Users
         fields = '__all__'
