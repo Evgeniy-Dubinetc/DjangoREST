52176ad51c8b2546774281967c34f7e7c3b5e900
Evgeniy-Dubinetc <edubinetc2@mail.ru>
2023-01-15 00:23:37 +0300

    lesson_3 models migrate

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import Users
+from os import *
 
-# Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=64)
+    repo_link = models.CharField(max_length=128)
+    user_name = models.ForeignKey(Users, on_delete=models.CASCADE)
+    
+    
+class TODO(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.CASCADE)
+    content = models.TextField
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Updated")
+    user_name = models.ForeignKey(Users, on_delete=models.CASCADE)
+    activate = models.BooleanField(("active"), default=True)
