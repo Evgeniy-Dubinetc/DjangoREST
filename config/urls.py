0f88a910421bbf9cb47078e39a446ac73918da04
Evgeniy-Dubinetc <edubinetc2@mail.ru>
2023-01-15 22:35:09 +0300

    Lesson3 alredy

--- config/urls.py
+++ config/urls.py
@@ -18,17 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UsersModelViewSet # new
+from todo.views import ProjectModelViewSet, TODOModelViewSet
 # from users.views import UsersAPIVieW # new
 
 router = DefaultRouter()
 # router.register('users', UsersAPIVieW)
 router.register('users', UsersModelViewSet)
+router.register('project', ProjectModelViewSet)
+router.register('todo', TODOModelViewSet)
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    # path('api/v1/userslist/', UsersAPIVieW.as_view()),
-    
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,24 @@
 # from rest_framework import serializers
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Users
+from rest_framework.serializers import ModelSerializer
+from todo.models import TODO, Project
+from users.models import Users
+
 
 
 # class UsersSerializer(serializers.ModelSerializer):
-class UsersModelSerializer(HyperlinkedModelSerializer):
+class UsersModelSerializer(ModelSerializer):
     class Meta:
         model = Users
+        fields = '__all__'
+
+
+class ProjectModelSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'  
+
+
+class TODOModelSerializer(ModelSerializer):
+    class Meta:
+        model = TODO
         fields = '__all__'
