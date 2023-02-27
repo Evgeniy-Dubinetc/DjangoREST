98679bc15011edd2dafccb0fc6546a38c9949a80
Evgeniy-Dubinetc <90556650+Evgeniy-Dubinetc@users.noreply.github.com>
2023-02-27 03:14:13 +0300

    lesson_11 ready

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,55 +1,40 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 
-const TodoItem = ({item}) => {
+const TodoItem = ({item, deleteTodo}) => {
     return (
         <tr>
-            <td>
-                {item.project_name}
-            </td>
-            <td>
-                {item.content}
-            </td>
-            <td>
-                {item.time_create}
-            </td>
-            <td>
-                {item.time_update}
-            </td>
-            <td>
-                {item.user_name}
-            </td>
-            <td>
-                {item.activate}
-            </td>
+            <td>{item.project_name}</td>
+            <td>{item.content}</td>
+            <td>{item.time_create}</td>
+            <td>{item.time_update}</td>
+            <td>{item.user_name}</td>
+            <td>{item.activate}</td>
+            <td><button onClick={()=>deleteTodo(item.id)} type='button'>Delete</button></td>
         </tr>
     )
 }
 
    
-const TodoList = ({items}) => {
+const TodoList = ({items, deleteTodo}) => {
     return (
+        <div>
         <table>
-            <th>
-                PROJECT_NAME
-            </th>
-            <th>
-                CONTENT
-            </th>
-            <th>
-                TIME_CREATE
-            </th>
-            <th>
-                TIME_UPDATE
-            </th>
-            <th>
-                USER_NAME
-            </th>
-            <th>
-                ACTIVATE
-            </th>
-            {items.map((item_) => <TodoItem item={item_} />)}
+            <tr>
+                <th>PROJECT_NAME</th>
+                <th>CONTENT</th>
+                <th>TIME_CREATE</th>
+                <th>TIME_UPDATE</th>
+                <th>USER_NAME</th>
+                <th>ACTIVATE</th>
+                <th></th>
+            </tr>
+            {items.map((item_) => <TodoItem item={item_} deleteTodo={deleteTodo} />)}
         </table>
+        <Link to='/todo/create'>Create</Link>     
+        </div>
+        
     )
 }
     
